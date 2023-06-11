a323cea1454e261d81027c1ad9f6f4b6367389fa
bashayer770 <beshow95@gmail.com>
2023-06-11 20:14:24 +0300

    auth 2

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,21 @@
+import { configure } from "@testing-library/react";
 import axios from "axios";
 
 const instance = axios.create({
   baseURL: "https://task-react-auth.herokuapp.com/api",
   // baseURL: "http://localhost:5000/api",
 });
+instance.interceptors.request.use(
+  (config) => {
+    localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `bearer ${token}`;
+    }
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
 
 export default instance;
